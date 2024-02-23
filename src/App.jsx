be7eee9a89f98e49313590782ee4b2d4cc1bab8e
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-02-23 13:53:01 +0530

    Add all files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Container from './Container.jsx'
+
+function App() {
+  
+  return (
+    <>
+    <h1 className='text-center text-gray-900 font-medium text-lg'>Password Generator</h1>
+    <div className='w-full h-screen flex justify-center bg-slate-950'>
+        <Container></Container>
+    </div>
+    </>
+  )
+}
+
+
+
+
+export default App
---
+++ src/Container.jsx
@@ -0,0 +1,82 @@
+import React, { useCallback, useEffect, useState ,useRef} from "react";
+
+
+const svg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+    />
+  </svg>
+);
+ 
+
+
+
+
+
+const Container = () => {
+  const [password,setPassword]=useState("")
+  const [size,setSize]=useState('6');
+  const [numbers,setNumbers]=useState(false);
+  const [chars,setChars]=useState(false);
+
+  const passwordRef=useRef(null);
+
+  const copyPassword=useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,999);
+    window.navigator.clipboard.writeText(password)
+  },[password])
+  
+
+ useEffect(()=>{
+  const alphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const nums="0123456789";
+  const Ch="~!@#$%^&*()_+=/?";
+  let sets=alphabets;
+  let tempPassword="";
+  for(let i=1;i<=size;i++){
+   if(numbers)
+   sets+=nums;
+    if(chars)
+    sets+=Ch;
+    let temp=Math.floor(Math.random()*sets.length);
+    tempPassword+=sets[temp]
+  }
+  setPassword(tempPassword);
+ },[numbers,chars,size])
+
+  return (
+    <div className="h-32 w-96 shadow-lg text-red-700 rounded-md bg-gray-700 text-center m-6  border-2 border-red-700 hover:-translate-y-1 scale-110 duration-100 cursor-pointer hover:bg-gray-500">
+        <div className="flex  mt-10 ml-14">
+        <div><input className="rounded-l-lg w-64 h-6 p-4 text-sm outline-none"type="text"  value={password} readOnly placeholder="Password" ref={passwordRef}/></div>
+        <div className="w-8 h-8 rounded-r-lg bg-blue-800 cursor-pointer hover:bg-white">
+            <button onClick={copyPassword}>
+            {svg}
+            </button>
+            </div>
+        </div>
+            <div className="mt-5 flex justify-center gap-x-2 text-red-950">
+              
+       <input  type="range" min={6} max={100} className="cursor-pointer" value={size} onChange={(e)=>setSize(e.target.value)} ref={passwordRef}/>Size({size})
+            
+       <input type="checkbox" defaultChecked={numbers} id="numberInput" onChange={()=>setNumbers(!numbers)}/><label htmlFor="numberInput">Numbers</label>
+             
+       <input type="checkbox" defaultChecked={chars} id="charInput" onChange={()=>setChars(!chars)}/> <label htmlFor="charInput">Chars</label>
+            </div>
+      
+    </div>
+  );
+};
+
+export default Container;
+
